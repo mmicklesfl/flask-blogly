325fcd99015e15ac735bc110d86ffca1b21cdd6b
Marissa <mickles.marissa@gmail.com>
2023-10-30 21:33:51 -0400

    completed further studies 2/3

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.hybrid import hybrid_property
 
 db = SQLAlchemy()
 
@@ -11,6 +12,14 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     image_url = db.Column(db.String(200), default='default_profile.jpg')
+
+    @hybrid_property
+    def full_name(self):
+        return self.last_name + ', ' + self.first_name
+
+    def get_full_name(self):
+        """Return the full name of the user in 'last, first' format."""
+        return f"{self.last_name}, {self.first_name}"
 
     def __repr__(self):
         return f"<User {self.id} {self.first_name} {self.last_name}>"
