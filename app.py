90bd1d2dbe6d8daecb2fdad5d61d1e8a884a4d3d
Marissa <mickles.marissa@gmail.com>
2023-11-02 22:04:53 -0400

    added post routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, User
+from models import db, User, Post
 
 app = Flask(__name__)
 
@@ -69,6 +69,53 @@ def users_delete(user_id):
     db.session.delete(user)
     db.session.commit()
     return redirect("/users")
+
+@app.route('/posts/new', methods=["GET", "POST"])
+def posts_new():
+    if request.method == "POST":
+        title = request.form['title']
+        content = request.form['content']
+        user_id = request.form['user_id']
+        new_post = Post(title=title, content=content, user_id=user_id)
+        db.session.add(new_post)
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+            flash('An error occurred while adding the post. Please try again.')
+            return render_template("posts/new.html", user_id=user_id)
+        return redirect(url_for('users_profile', user_id=user_id))
+    else:
+        user_id = request.args.get('user_id')
+        if not user_id:
+            flash('No user specified for the new post.')
+            return redirect(url_for('index'))
+        return render_template("posts/new.html", user_id=user_id)
+
+@app.route('/posts/<int:post_id>')
+def posts_show(post_id):
+    """Show a post."""
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/show.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["GET", "POST"])
+def posts_edit(post_id):
+    """Edit a post."""
+    post = Post.query.get_or_404(post_id)
+    if request.method == "POST":
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect(f'/posts/{post_id}')
+    return render_template('posts/edit.html', post=post)
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_delete(post_id):
+    """Delete a post."""
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect("/posts")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,17 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.hybrid import hybrid_property
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    """User in the Blogly application."""
-
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     image_url = db.Column(db.String(200), default='default_profile.jpg')
+    posts = db.relationship('Post', back_populates='user', lazy='dynamic')
 
     @hybrid_property
     def full_name(self):
@@ -23,3 +22,13 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.id} {self.first_name} {self.last_name}>"
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='posts')
